b662345bef0857461160cb5b889c4aa958c28dd8
fnascimentosilva <fabricionsilva26@gmail.com>
2024-10-08 23:29:32 -0400

    inserindo repo no github

---
+++ 2_fundamentos/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Welcome from './components/Welcome'
+
+function App() {
+ 
+
+  return (
+   
+    <Welcome></Welcome> 
+  
+  )
+}
+
+export default App
---
+++ 2_fundamentos/src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+function Welcome(){
+const name = "Fabricio";
+
+    return(
+        <>Meu primeiro componente e meu nome é {name}</>
+    )
+}
+
+export default Welcome;
