ca843f28eb352ca4ed17d27d6cbeb272c0632c45
fnascimentosilva <fabricionsilva26@gmail.com>
2024-10-12 21:11:06 -0400

    aplicando os principios de useState

---
+++ 2_fundamentos/src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Counter(){
+    const [count, setCount] = useState(1);
+    return(
+        <div>
+            <p>Você clicou {count} vezes.</p>
+            <button onClick={()=>setCount(count+1)
+            }>Clique</button>
+        </div>
+    )
+};
+
+export default Counter;
