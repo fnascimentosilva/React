77d487828edebd2974fcffc8eced9dda5adb984b
fnascimentosilva <fabricionsilva26@gmail.com>
2024-10-21 10:07:15 -0400

    mudando funcao reducer

--- 3_hooks/src/components/UseReducer.jsx
+++ 3_hooks/src/components/UseReducer.jsx
@@ -1,9 +1,34 @@
+import { useReducer } from "react"
+//Definicao de estado inicial
+const estadoInicial = { contador: 0 }
 
+//Funcao reducer que define como as acoes atualizam o estado
+function reducer(estado, acao) {
 
-function UseReducer() {
-  return (
-    <div>UseReducer</div>
-  )
+    switch (acao.tipo) {
+        case "incrementar":
+            return { contador: estado.contador + 1 };
+        case 'decrementar':
+            return { contador: estado.contador - 1 };
+        case 'resetar':
+            return { contador: 0 }
+        default:
+            throw new Error("Acao nao suportada");
+        
+    }
+    
 }
 
-export default UseReducer
+function Contador(){
+
+    const [estado, dispatch] = useReducer(reducer, estadoInicial);
+
+    return(
+        <div>
+            <p>Contagem:{estado.contador}</p>
+            <button onClick={()=> dispatch({tipo: "incrementar"})}>Incrementar</button>
+        </div>
+    )
+}
+
+export default Contador;
