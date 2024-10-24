3e2865c74bbed691585fc5bd9f76a1f336523f5f
fnascimentosilva <fabricionsilva26@gmail.com>
2024-10-23 23:09:15 -0400

    reseolvendo calculo fibonacci usando useMemo

--- 3_hooks/src/components/Exercises.jsx
+++ 3_hooks/src/components/Exercises.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import {useEffect, useMemo } from "react";
 
 /* Exercicio 1: Uso de useEffect para sincronização de dados
 Enunciado: Você precisa exibir informações de um usuário que são recebidas via props de um componente.
@@ -22,14 +22,42 @@ function ExibirDadosUser({Info}){
     )
 }
 
+
+/* Exercício 2: Memorizando cálculos com UseMemo
+Enunciado: Suponha que você tem um componente que realiza um cálculo pesado, como uma funcao de fibonacci que é chamada com um número específico.
+Use o hook UseMemo para evitar que o calculo seja refeito desnecessariamente. */
+
+function fibonacci(n){
+    if(n <= 1){
+        return n;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function FibCalculator ({num}) {
+    const fibResult = useMemo(() => fibonacci(num), [num]);
+
+    return(
+        <div>
+            <p>
+                Fibonacci de {num} é {fibResult}
+            </p>
+        </div>
+    )
+    
+}
+
 function Exercises() {
 
     const userInfo = {name: "Fabrício", jobTitle: "Programador"};
+    const num = 10;
 
     return (
         <div>
             <h1>Exercício 1</h1>
             <ExibirDadosUser Info = {userInfo} />
+            <FibCalculator num = {num}/>
         </div>
     )
 }
