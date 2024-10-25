4799a5a7ebbde315908ff231e284436de39c6b1e
fnascimentosilva <fabricionsilva26@gmail.com>
2024-10-24 23:34:24 -0400

    add os componentes que serão utilizados na main

--- 4_projeto_to_do_list/src/App.jsx
+++ 4_projeto_to_do_list/src/App.jsx
@@ -1,11 +1,15 @@
+import TaskInput from "./components/TaskInput"
+import TaskList from "./components/TaskList"
 
-import './App.css'
 
 function App() {
   
 
   return (
     <>
+      <h1>Lista de tarefas</h1>
+      <TaskInput/>
+      <TaskList/>
       
     </>
   )
