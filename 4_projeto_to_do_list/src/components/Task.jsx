489f8ee032a5d22db15b7e9d533c46e8e03ab981
fnascimentosilva <fabricionsilva26@gmail.com>
2024-10-24 23:34:06 -0400

    add estrutura inicial dos componentes

---
+++ 4_projeto_to_do_list/src/components/Task.jsx
@@ -0,0 +1,12 @@
+
+function Task() {
+  return (
+    <li>
+        <span>Texto da tarefa</span>
+        <button>remover</button>
+    </li>
+    
+  )
+}
+
+export default Task
---
+++ 4_projeto_to_do_list/src/components/TaskInput.jsx
@@ -0,0 +1,11 @@
+
+function TaskInput() {
+  return (
+    <form >
+        <input type="text"  placeholder="Adicione aqui a atividade"/>
+        <button type="submit">adicionar</button>
+    </form>
+  )
+}
+
+export default TaskInput
