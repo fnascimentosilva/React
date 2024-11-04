c48bcfe9ce112e9d596ce290152c7c9d5963ff23
fnascimentosilva <fabricionsilva26@gmail.com>
2024-11-04 00:04:02 -0400

    salvando as alteracoes no localstorage

--- 4_projeto_to_do_list/src/App.jsx
+++ 4_projeto_to_do_list/src/App.jsx
@@ -5,32 +5,37 @@ import TaskList from "./components/TaskList"
 
 
 function App() {
+  //tranforma Json em array pra armazenar no localstorage
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
   
-  const [tasks, setTasks] = useState([]);
+  //transforma o array em uma string
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   //adiciona uma nova tarefa
   const addTask = (task) => {
 
     //id, text, done
-    setTasks([...tasks, {id:Date.now(), text: task, done: false}]);
+    setTasks([...tasks, { id: Date.now(), text: task, done: false }]);
   }
 
   //deleta ao clicar no botao de remover
   function deleteTask(taskId) {
-    setTasks (tasks.filter((task) => task.id !== taskId));
+    setTasks(tasks.filter((task) => task.id !== taskId));
   }
 
   //inserir o riscado na tarefa feita
-  function toggleTaskDone (taskId){
-    setTasks(tasks.map((task) => task.id === taskId ? {...task, done: !task.done} : task))
+  function toggleTaskDone(taskId) {
+    setTasks(tasks.map((task) => task.id === taskId ? { ...task, done: !task.done } : task))
   }
 
   return (
     <>
       <h1>Lista de tarefas</h1>
-      <TaskInput onAddTask = {addTask}/>
-      <TaskList tasks = {tasks} onDeleteTask = {deleteTask} onToggleTaskDone = {toggleTaskDone}/>
-      
+      <TaskInput onAddTask={addTask} />
+      <TaskList tasks={tasks} onDeleteTask={deleteTask} onToggleTaskDone={toggleTaskDone} />
+
     </>
   )
 }
