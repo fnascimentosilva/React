9b450bbb349152c52d92ea5ec1d498bdcf8e5a0d
fnascimentosilva <fabricionsilva26@gmail.com>
2024-11-04 00:21:35 -0400

    add uma mensagem caso nao haja atividades cadastradas

--- 4_projeto_to_do_list/src/components/TaskList.jsx
+++ 4_projeto_to_do_list/src/components/TaskList.jsx
@@ -1,7 +1,12 @@
 import Task from "./Task"
 
 function TaskList({ tasks, onDeleteTask, onToggleTaskDone }) {
+
+  if(tasks.length === 0){
+   return <p>Não há tarefas cadastradas!</p>
+  }
   return (
+    
     <ul>
       {tasks.map((task) => (
         <Task key={task.id} task={task} onDelete = {() => onDeleteTask(task.id)} onToggleDone = {() => onToggleTaskDone(task.id)}/>
