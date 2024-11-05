73df8dc4e81d9de7614521ddfd793c592b29caa9
fnascimentosilva <fabricionsilva26@gmail.com>
2024-11-05 00:33:36 -0400

    aprendendo a usar Routes, route e Link

--- 5_react_router/src/App.jsx
+++ 5_react_router/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, Link, NavLink } from 'react-router-dom'
 import './App.css'
+import Home from './assets/components/Home'
+import About from './assets/components/About'
+import Contact from './assets/components/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      {/* Setup e configuracao inicial */ }
+      <nav>
+        <ul>
+          <li><Link to="/">Inicio</Link></li>
+          <li><Link to="/about">Sobre</Link></li>
+          <li><Link to="/contact">Contato</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <p>Rodapé</p>
     </>
   )
 }
