9ae838fee081af586da39ef6eb6bd82986f3bd6a
fnascimentosilva <fabricionsilva26@gmail.com>
2025-04-17 00:10:05 -0400

    resolvendo exercicios React

--- 2_fundamentos/src/components/Exercises.jsx
+++ 2_fundamentos/src/components/Exercises.jsx
@@ -7,10 +7,7 @@ function Greetings({ name }) {
     */
     return (
         <div>
-            {/* Se o name tiver vazio não imprime nada na tela, se o name tiver valor irá imprimir a mensagem Olá, name */}
-            {/* {name && <p> Olá, {name}! </p>} */}
-            {/* Operacao ternária */}
-            {name ? <p> Olá, {name}! </p> : <p> Insira um nome válido pra prosseguir! </p>}
+            {name ? <p>Seja bem vindo, {name}</p> : <p>Escreva um nome válido para inciar</p>}
         </div>
     )
 }
@@ -19,13 +16,13 @@ function Counter() {
     /* Enunciado Exercíico 2:
     Crie um componente chamado Counter que renderiza um botão e um texto indicando o número de vezes que o botão foi clcado.
     Por exemplo, se o botão foi clicado 3 vezes, o texto deve mostrar "Você clicou 3 vezes no botão". Inicialize o contador com 0 e incremente a cada clique novo. */
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
 
     return (
-        <div>
-            {count > 1 ? <p>Você clicou {count} vezes no botão</p> : <p>Você clicou {count} vez no botão</p>}
 
-            <button onClick={() => setCount(count + 1)}>Clique</button>
+        <div>
+            <button onClick={() => setCount(count + 1)}>Clique aqui</button>
+            <p>O botao foi clicado {count} vez</p>
         </div>
     )
 }
@@ -34,16 +31,17 @@ const TaskList = ({ tasks }) => {
     /* Enunciado Exercício 3:
     Crie um componente chamado TaskList que aceita uma props tasks, que é um array de objetos com id e text.
     Renderize uma lista ordenada de tarefas, mostrando o texto de cada tarefa. Se não houver tarefas, exiba a nebsagen "Não há tarefas a mostrar!"*/
-    if (tasks.length === 0) {
-        return <p>Não há tarefas a mostrar!</p>;
-    }
+
+    if (tasks.length === 0) return <p>Não há tarefas a mostrar!</p>
 
     return (
-        <ul>
-            {tasks.map((task) => (
-                <li key={task.id}>{task.text}</li>
+        <>
+            <ul>{tasks.map((task) => (
+                <li key = {task.id}>{task.text}</li>
             ))}
-        </ul>
+
+            </ul>
+        </>
     )
 
 
@@ -55,12 +53,13 @@ function Exercises() {
     const tasks = [
         { id: 1, text: "levantar cedo" },
         { id: 2, text: "tomar banho" },
+        { id: 3, text: "estudar" },
     ];
 
     return (
         <div>
             <h2>Exercício 1</h2>
-            <Greetings name="Douglas" />
+            <Greetings name="Fabrício" />
 
             <h2>Exercício 2</h2>
             <Counter />
